a1661cffd455a41f9021584f9380375baaa87fb1
Eze Onyebuchi <eze4acme@gmail.com>
2023-08-07 21:55:36 +0100

    update

---
+++ data.js
@@ -0,0 +1,33 @@
+export const quizQuestions = [
+  {
+    question: "What is the capital of France?",
+    options: ["London", "Berlin", "Paris", "Rome"],
+    correctAnswer: "Paris"
+  },
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["Mars", "Venus", "Jupiter", "Saturn"],
+    correctAnswer: "Mars"
+  },
+  {
+    question: "Who wrote the play 'Romeo and Juliet'?",
+    options: ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
+    correctAnswer: "William Shakespeare"
+  },
+  {
+    question: "What is the largest mammal in the world?",
+    options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+    correctAnswer: "Blue Whale"
+  },
+  {
+    question: "Which gas do plants use for photosynthesis?",
+    options: ["Oxygen", "Nitrogen", "Carbon Dioxide", "Hydrogen"],
+    correctAnswer: "Carbon Dioxide"
+  }
+];
+// utils.js
+const add = (a, b) => a + b;
+
+export default add;
+
+// export default quizQuestions;
---
+++ index.js
@@ -0,0 +1,7 @@
+import quizQuestions from './data'
+console.log(quizQuestions);
+// main.js
+import add from './utils';
+
+const result = add(5, 3);
+console.log(result); // Output: 8
